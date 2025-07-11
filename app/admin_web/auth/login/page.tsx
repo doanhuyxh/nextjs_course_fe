441921347049f423d73c664b87531c995121d049
doanhuyxh <doanhuyxh@gmail.com>
2025-07-11 16:38:29 +0700

    up fix tai khoan admin

--- app/admin_web/auth/login/page.tsx
+++ app/admin_web/auth/login/page.tsx
@@ -8,7 +8,7 @@ import axiosInstance from '@/libs/configs/axiosAdminConfig';
 import { ResponseData } from '@/libs/types';
 
 export default function Login() {
-    const [email, setEmail] = useState<string>('admin@gmail.comin');
+    const [email, setEmail] = useState<string>('admin@gmail.com');
     const [password, setPassword] = useState<string>('dantrivn');
     const router = useRouter()
 
